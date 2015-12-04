331a2831c06d99df4df88ebe935e8f8e9bed123c
Michiel Lucas <michiel.lucas@one-agency.be>
2015-12-04 15:13:20 +0100

    Format and test ML

--- src/Form/ColumnDeleteForm.php
+++ src/Form/ColumnDeleteForm.php
@@ -18,45 +18,45 @@ use Drupal\Core\Url;
  */
 class ColumnDeleteForm extends ContentEntityConfirmFormBase {
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getQuestion() {
-        return $this->t('Are you sure you want to delete entity %name?', array('%name' => $this->entity->label()));
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function getQuestion() {
+    return $this->t('Are you sure you want to delete entity %name?', array('%name' => $this->entity->label()));
+  }
 
-    /**
-     * {@inheritdoc}
-     *
-     * If the delete command is canceled, return to the contact list.
-     */
-    public function getCancelURL() {
-        return new Url('entity.datatank_column.collection');
-    }
+  /**
+   * {@inheritdoc}
+   *
+   * If the delete command is canceled, return to the contact list.
+   */
+  public function getCancelURL() {
+    return new Url('entity.datatank_column.collection');
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getConfirmText() {
-        return $this->t('Delete');
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfirmText() {
+    return $this->t('Delete');
+  }
 
-    /**
-     * {@inheritdoc}
-     *
-     * Delete the entity and log the event. log() replaces the watchdog.
-     */
-    public function submitForm(array &$form, FormStateInterface $form_state) {
-        $entity = $this->getEntity();
-        $entity->delete();
+  /**
+   * {@inheritdoc}
+   *
+   * Delete the entity and log the event. log() replaces the watchdog.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $entity = $this->getEntity();
+    $entity->delete();
 
-        \Drupal::logger('datatank')->notice('@type: deleted %title.',
-            array(
-                '@type' => $this->entity->bundle(),
-                '%title' => $this->entity->label(),
-            ));
-        $form_state->setRedirect('entity.datatank_column.collection');
-    }
+    \Drupal::logger('datatank')->notice('@type: deleted %title.',
+      array(
+        '@type' => $this->entity->bundle(),
+        '%title' => $this->entity->label(),
+      ));
+    $form_state->setRedirect('entity.datatank_column.collection');
+  }
 
 }
 
