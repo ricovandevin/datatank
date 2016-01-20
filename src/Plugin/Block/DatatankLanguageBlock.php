74f06f4a924d29ca00f6c702ffa0ad479e5c19a9
michiel lucas <info@michiellucas.be>
2016-01-20 16:04:05 +0100

    Language block datatank

---
+++ src/Plugin/Block/DatatankLanguageBlock.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\datatank\Plugin\Block\DatatankLanguageBlock.
+ */
+
+namespace Drupal\datatank\Plugin\Block;
+
+use \Drupal\language\Plugin\Block\LanguageBlock;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+
+/**
+ * Provides a 'Language switcher' block.
+ *
+ * @Block(
+ *   id = "datatank_language_block",
+ *   admin_label = @Translation("DATATANK Language switcher"),
+ *   category = @Translation("Datatank"),
+ * )
+ */
+class DatatankLanguageBlock extends LanguageBlock implements ContainerFactoryPluginInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = parent::build();
+
+    $entity = \Drupal::request()->attributes->get('node');
+    if ($entity) {
+      if (!$entity->hasTranslation('nl')) {
+        unset($build['#links']['nl']);
+      }
+
+      if (!$entity->hasTranslation('EN')) {
+        unset($build['#links']['en']);
+      }
+
+
+    }
+
+    return $build;
+  }
+
+}
