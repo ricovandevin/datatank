34d791d276f45da38c15075069aded8d08e6c8f9
michiel lucas <info@michiellucas.be>
2016-02-03 14:41:37 +0100

    Datatank download form

--- src/Form/DatasetDownload.php
+++ src/Form/DatasetDownload.php
@@ -51,123 +51,130 @@ class DatasetDownload extends FormBase {
       '#markup' => '<h2>' . $this->t('Filter data') . '</h2>',
     ];
 
-    $form['filter']['location'] = [
-      '#type' => 'radios',
-      '#title' => $this->t('Location'),
-      '#options' => [
-        'no' => $this->t('Not filtered'),
-        'region' => $this->t('Tourist region'),
-        'town' => $this->t('Town'),
-        'coor' => $this->t('At coordinates (Lambert 72)'),
-      ],
-      '#default_value' => 'no'
-    ];
+    if ($datatank_dataset->canBeFiltered()) {
+      $form['filter']['location'] = [
+        '#type' => 'radios',
+        '#title' => $this->t('Location'),
+        '#options' => [
+          'no' => $this->t('Not filtered'),
+          'region' => $this->t('Tourist region'),
+          'town' => $this->t('Town'),
+          'coor' => $this->t('At coordinates (Lambert 72)'),
+        ],
+        '#default_value' => 'no'
+      ];
 
-    $regions_raw = \Drupal::entityManager()
+      $regions_raw = \Drupal::entityManager()
         ->getStorage('taxonomy_term')
         ->loadTree('region');
 
-    $regions = [];
+      // Needed for Select 2 empty option
+      $regions = ['' => ''];
 
-    $regions[0] = t('All regions');
+      foreach ($regions_raw as $reg) {
+        $regions[$reg->tid] = $reg->name;
+      }
 
-    foreach ($regions_raw as $reg) {
-      $regions[$reg->tid] = $reg->name;
+      $form['filter']['region'] = [
+        '#type' => 'select',
+        '#title' => $this->t('Tourist region'),
+        '#options' => $regions,
+        '#default_value' => 0,
+        '#states' => array(
+          'visible' => array(
+            ':input[name="location"]' => array('value' => 'region'),
+          ),
+        ),
+        '#attributes' => [
+          'data-placeholder' => t('All regions')
+        ],
+      ];
+
+      $postal_codes_raw = array_map('str_getcsv', file(\Drupal::root() . '/' . drupal_get_path("module", 'datatank') . '/zipcodes.csv'));
+      // Needed for Select 2 empty option
+      $postal_codes = ['' => ''];
+
+      foreach ($postal_codes_raw as $postal_code_raw) {
+        //$postal_codes[strtolower($postal_code_raw[1])] = $postal_code_raw[0] . '-' . $postal_code_raw[1];
+        $postal_codes[strtolower($postal_code_raw[1])] = $postal_code_raw[1];
+      }
+
+      $form['filter']['town'] = [
+        '#type' => 'select',
+        '#title' => $this->t('Town'),
+        '#options' => $postal_codes,
+        '#default_value' => 0,
+        '#states' => array(
+          'visible' => array(
+            ':input[name="location"]' => array('value' => 'town'),
+          ),
+        ),
+        '#attributes' => [
+          'data-placeholder' => t('Town')
+        ],
+      ];
+
+      $form['filter']['coor'] = [
+        '#type' => 'container',
+        '#tree' => TRUE,
+        '#states' => array(
+          'visible' => array(
+            ':input[name="location"]' => array('value' => 'coor'),
+          ),
+        ),
+        '#attributes' => [
+          'class' => ['dataset-download__filters-coordinates']
+        ],
+      ];
+
+      $config = \Drupal::configFactory()->getEditable('datatank.settings');
+      $url = Url::fromUserInput($config->get('datatank_link_lambert72'));
+      $external_link = '<span class="info-link">' . \Drupal::l('?', $url) . '</span>';
+
+      $form['filter']['coor']['info'] = [
+        '#type' => 'markup',
+        '#markup' => '<div class="dataset-download__filters-coordinates_label"><b>1. </b>' . t('Lambert 72 coordinates') . '</div>' . $external_link,
+      ];
+
+      $form['filter']['coor']['x_coord'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('X='),
+        '#placeholder' => t('Ex. 188600'),
+      ];
+
+      $form['filter']['coor']['y_coord'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Y='),
+        '#placeholder' => t('Ex. 188600'),
+      ];
+
+      $form['filter']['coor']['radiusinfo'] = [
+        '#markup' => '<div class="dataset-download__filters-radius_label"><b>2. </b>' . t('Radius (in meters)') . '</div>',
+      ];
+
+      $form['filter']['coor']['radius'] = [
+        '#type' => 'textfield',
+        '#placeholder' => t('Ex. 5000'),
+      ];
+
+      $form['filter']['labels'] = [
+        '#type' => "checkboxes",
+        '#title' => $this->t('Labels'),
+        '#options' => [
+          'bicycle_friendly' => $this->t('Bicycle friendly'),
+          'green_key' => $this->t('Green'),
+          'accessibility' => $this->t('Accessibility'),
+        ]
+      ];
     }
-
-    $form['filter']['region'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Tourist region'),
-      '#options' => $regions,
-      '#default_value' => 0,
-      '#states' => array(
-        'visible' => array(
-          ':input[name="location"]' => array('value' => 'region'),
-        ),
-      ),
-    ];
-
-    $postal_codes_raw = array_map('str_getcsv', file(\Drupal::root() . '/' . drupal_get_path("module", 'datatank') . '/zipcodes.csv'));
-    $postal_codes = [];
-
-    $postal_codes[0] = t('All towns');
-
-    foreach ($postal_codes_raw as $postal_code_raw) {
-      $postal_codes[strtolower($postal_code_raw[1])] = $postal_code_raw[0] . '-' . $postal_code_raw[1];
-    }
-
-    $form['filter']['town'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Town'),
-      '#options' => $postal_codes,
-      '#default_value' => 0,
-      '#states' => array(
-        'visible' => array(
-          ':input[name="location"]' => array('value' => 'town'),
-        ),
-      ),
-    ];
-
-    $form['filter']['coor'] = [
-      '#type' => 'container',
-      '#tree' => TRUE,
-      '#states' => array(
-        'visible' => array(
-          ':input[name="location"]' => array('value' => 'coor'),
-        ),
-      ),
-      '#attributes' => [
-        'class' => ['dataset-download__filters-coordinates']
-      ],
-    ];
-
-    $config = \Drupal::configFactory()->getEditable('datatank.settings');
-    $url = Url::fromUserInput($config->get('datatank_link_lambert72'));
-    $external_link = '<span class="info-link">' . \Drupal::l('?', $url) . '</span>';
-
-    $form['filter']['coor']['info'] = [
-      '#type' => 'markup',
-      '#markup' => '<div class="dataset-download__filters-coordinates_label"><b>1. </b>' . t('Lambert 72 coordinates') . '</div>' . $external_link,
-    ];
-
-    $form['filter']['coor']['x_coord'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('X='),
-      '#placeholder' => t('Ex. 188600'),
-    ];
-
-    $form['filter']['coor']['y_coord'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Y='),
-      '#placeholder' => t('Ex. 188600'),
-    ];
-
-    $form['filter']['coor']['radiusinfo'] = [
-      '#markup' => '<div class="dataset-download__filters-radius_label"><b>2. </b>' . t('Radius (in meters)') . '</div>',
-    ];
-
-    $form['filter']['coor']['radius'] = [
-      '#type' => 'textfield',
-      '#placeholder' => t('Ex. 5000'),
-    ];
-
-    $form['filter']['labels'] = [
-      '#type' => "checkboxes",
-      '#title' => $this->t('Labels'),
-      '#options' => [
-        'bicycle_friendly' => $this->t('Bicycle friendly'),
-        'green_key' => $this->t('Green'),
-        'accessibility' => $this->t('Accessibility'),
-      ]
-    ];
 
     // laatste wijziging
     $form['filter']['timestamp'] = [
       '#type' => 'date',
       '#title' => $this->t('Last changed'),
-      '#description' => $this->t('Format: @date', array('@date' => format_date(time(), 'custom', 'Y-m-d')))
-        //'#default_value' => array('year' => 2010, 'month' => 2, 'day' => 12),
-        //'#default_value' => '2010-02-01',
+      '#description' => $this->t('Format: @date', array('@date' => format_date(time(), 'custom', 'Y-m-d'))),
+      //'#default_value' => array('year' => 2010, 'month' => 2, 'day' => 12),
+      '#default_value' => '',
     ];
 
     $form['filter']['submit'] = [
@@ -209,8 +216,8 @@ class DatasetDownload extends FormBase {
       case 'region':
         if ($form_state->hasValue('region')) {
           $term = \Drupal::entityManager()
-              ->getStorage('taxonomy_term')
-              ->load($form_state->getValue('region'));
+            ->getStorage('taxonomy_term')
+            ->load($form_state->getValue('region'));
           $query['region'] = $term->get('name')->value;
         }
         break;
@@ -275,7 +282,7 @@ class DatasetDownload extends FormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    if ($form_state->hasValue('timestamp')) {
+    if ($form_state->hasValue('timestamp') && $form_state->getValue('timestamp') != '') {
       $timestamp = $form_state->getValue('timestamp');
       if (strtotime($timestamp)) {
         $test_date = format_date(strtotime($timestamp), 'custom', 'Y-m-d');
